342629b260aef5bbf6ac06e55d212cd2069c4667
Alberto Fernandez <fernandez.gandarias@gmail.com>
2017-06-24 10:17:24 +0200

    first exercise done

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,23 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/random', (req, res) => {
+
+    // Retrieve a random chuck joke
+client.getRandomJoke()
+  .then((response) => {
+    res.render('index', response);
+  }).catch((err) => {
+    // handle error
+  });
+ 
+});
+
+app.listen (3000, () =>{
+	console.log('Listening on port 3000!')
+});
